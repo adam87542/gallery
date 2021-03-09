fb8f40a3e898c0d5a615176dc154c8ac14b0f5f7
adam shamis <shuka.fred@gmail.com>
2021-03-09 14:36:55 +0200

    Print dev name and current time

--- Gallery-files/Gallery.cpp
+++ Gallery-files/Gallery.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <string>
+#include <chrono> 
+#include <ctime> 
 #include "MemoryAccess.h"
 #include "AlbumManager.h"
-
 
 int getCommandNumberFromUser()
 {
@@ -27,7 +28,11 @@ int getCommandNumberFromUser()
 	
 	return std::atoi(input.c_str());
 }
-
+void Print_dev_name_and_curr_time()
+{
+	auto currtime = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+	std::cout << "by adam shamis version 1.0.1, current time : " << ctime(&currtime) << std::endl;
+}
 int main(void)
 {
 	// initialization data access
@@ -38,7 +43,8 @@ int main(void)
 
 
 	std::string albumName;
-	std::cout << "Welcome to Gallery!" << std::endl;
+	std::cout << " Welcome to Gallery! ";
+	Print_dev_name_and_curr_time();
 	std::cout << "===================" << std::endl;
 	std::cout << "Type " << HELP << " to a list of all supported commands" << std::endl;
 	
